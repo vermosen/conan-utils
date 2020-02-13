e51634489ec153e2e54d1795259519660a03ae1e
vermosen.jm@free.fr <vermosen@yahoo.com>
2020-02-13 10:29:59 -0500

    added rcpp/r-cran recipes

---
+++ r-cran/3.6.0/conanfile.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+
+class RConan(ConanFile):
+    name = 'r-cran'
+    version = '3.6.0'
+    description = 'the R statistical launguage.'
+    license = 'Apache License Version 2.0'
+    url = 'https://cran.r-project.org/src/base'
+    homepage = 'https://cran.r-project.org'
+    author = 'vermosen@yahoo.com'
+
+    settings = 'os', 'compiler', 'build_type', 'arch', 'cppstd'
+
+    # note: Rshlib is required for using with R-studio
+    options = { "shared": [True, False],
+                "fPIC": [True, False],
+                "Rshlib": [True, False],
+                "lapack": [True, False],
+                "blas": ["R", "mkl", "openblas"],
+                "lto": [True, False]
+    }
+
+    default_options = {
+        "shared": True ,
+        "fPIC": True ,
+        "Rshlib": False,
+        "lapack": False,
+        "blas": "R",
+        "lto": False
+    }
+    
+    generators = "cmake"
+    _source_subfolder = 'src'
+
+    def configure(self):
+        # from https://cran.r-project.org/doc/manuals/r-release/R-admin.html
+        self.options["pcre"].with_utf = True
+        self.options["prce"].with_jit = True
+        self.options["prce"].with_unicode_properties = True
+        self.options["pcre"].build_pcrecpp = False
+        self.options["libcurl"].shared = True
+    def build_requirements(self):
+        self.build_requires("libcurl/7.64.1@%s/%s" % (self.user, self.channel))
+        self.build_requires("bzip2/1.0.6@%s/%s" % (self.user, self.channel))
+        self.build_requires("lzma/5.2.4@%s/%s" % (self.user, self.channel))
+        self.build_requires("pcre/8.41.0@%s/%s" % (self.user, self.channel))
+        self.build_requires("zlib/1.2.11@%s/%s" % (self.user, self.channel))
+
+        if self.options.lapack:
+            self.build_requires("lapack/3.8.0@rubedo/stable")
+
+    def source(self):
+        #https://cran.r-project.org/src/base/R-3/R-3.5.0.tar.gz
+        v = self.version.split('.')
+        path = '%s/R-%s' % (self.url, v[0])
+        filename = 'R-%s.%s.%s.tar.gz' % (v[0], v[1], v[2])
+        url = '%s/%s' % (path, filename)
+        
+        self.output.info('retrieving archive from %s' % url)
+        tools.download(url, filename)
+        tools.unzip(filename)
+        shutil.move("R-%s" % (self.version), self._source_subfolder)
+        
+    def build(self):
+        libcurl = self.deps_cpp_info["libcurl"].rootpath
+
+        self.output.info('curl directory set to %s'% libcurl)
+        self.build_dir = os.path.join(os.getcwd(), self._source_subfolder)
+        with tools.chdir(self._source_subfolder):
+            env_build = AutoToolsBuildEnvironment(self)
+            env_build_vars = env_build.vars # edit here
+
+            args = ['--with-readline=no', '--with-x=no']
+            if self.options.lto:
+                args += ['--enable-lto=yes']
+            if self.options.lapack:
+                args += ['--with-lapack=%s/lib' % self.deps_cpp_info["lapack"].rootpath]
+            if self.options.Rshlib:
+                args += ['--enable-R-shlib=yes']
+            else:
+                args += ['--enable-R-shlib=no']
+            if self.options.shared:
+                args += ['--enable-R-static-lib=no']
+            else:
+                args += ['--enable-R-static-lib=yes']
+
+            env_build.fpic = self.options.fPIC
+            env_build.configure(vars=env_build_vars, args=args)
+            env_build.make()
+
+    def package(self):
+        self.build_dir = os.path.join(os.getcwd(), self._source_subfolder)
+        with tools.chdir(self.build_dir):
+            #need to manually install
+            self.copy('*.h' , dst='include', src='src/include', keep_path=True)
+            self.copy('*.so' , dst='lib', src='src/lib', keep_path=False)
+            self.copy('*.a' , dst='lib', src='src/lib', keep_path=False)
+            self.copy('*' , dst='bin', src='src/bin', keep_path=True)
+
+    def package_info(self):
+        self.env_info.libs = tools.collect_libs(self)
---
+++ rcpp/1.0.1/conanfile.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+from conans import ConanFile, tools
+
+class RCppConan(ConanFile):
+    name = 'rcpp'
+    version = '1.0.1'
+    description = 'the rcpp R-package.'
+    license = 'Apache License Version 2.0'
+    url = 'https://github.com/RcppCore/Rcpp'
+    homepage = 'https://github.com/RcppCore/Rcpp'
+    author = 'jvermosen@hap-capital.com'
+
+    settings = 'os', 'compiler', 'build_type', 'arch', 'cppstd'
+    generators = "cmake"
+    _source_subfolder = 'src'
+
+    def configure(self):
+        pass
+
+    def build_requirements(self):
+        self.build_requires("r-cran/3.5.0@rubedo/stable")
+
+    def source(self):
+        #https://github.com/RcppCore/Rcpp/archive/1.0.1.tar.gz
+        # becomes Rcpp-1.0.1 folder
+        archive = '%s.tar.gz' % self.version
+        tools.download('%s/archive/%s' % (self.url, archive), '%s.tar.gz' % self.version)
+        tools.unzip('%s.tar.gz' % self.version)
+        folder = 'Rcpp-%s' % self.version
+        shutil.move(folder, self._source_subfolder)
+
+    def build(self):
+        pass
+
+    def package(self):
+        self.copy('*.h' , dst='include/Rcpp', src='src/inst/include/Rcpp', keep_path=True)
+        self.copy('*.hpp' , dst='include/Rcpp', src='src/inst/include/Rcpp', keep_path=True)
+        self.copy('Rcpp.h', dst='include', src='src/inst/include')
+        self.copy('RcppCommon.h', dst='include', src='src/inst/include')
+
+    def package_info(self):
+        self.env_info.libs = tools.collect_libs(self)
