914b99496c00f2540d226b3bb40ff2307cc84026
Jean-Mathieu Vermosen <vermosen@yahoo.com>
2020-02-28 14:34:48 -0500

    up

--- eigen/3.3.7/conanfile.py
+++ eigen/3.3.7/conanfile.py
@@ -14,34 +14,19 @@ class EigenConan(ConanFile):
                    numerical solvers, and related algorithms."
     license = "Mozilla Public License Version 2.0"
     no_copy_source = True
-    settings = 'arch', 'cppstd', 'compiler'
+    settings = 'arch', 'cppstd', 'compiler', 'build_type'
 
-    options = {"EIGEN_USE_BLAS":           [True, False]
-             , "EIGEN_USE_LAPACKE":        [True, False]
-             , "EIGEN_USE_LAPACKE_STRICT": [True, False]
-             , "EIGEN_USE_MKL_VML":        [True, False]
-             , "EIGEN_USE_MKL_ALL":        [True, False]
+    options = {
+        "test": [True, False]
     }
 
-    default_options = "EIGEN_USE_BLAS=False",\
-        "EIGEN_USE_LAPACKE=False",\
-        "EIGEN_USE_LAPACKE_STRICT=False",\
-        "EIGEN_USE_MKL_VML=False",\
-        "EIGEN_USE_MKL_ALL=False"
+    default_options = "test=False"
 
-    _build_subfolder = 'build'
+    _build_subfolder  = 'build'
     _source_subfolder = 'src'
 
     def configure(self):
-
-        if self.options.EIGEN_USE_BLAS == True:
-            self.requires("lapack/3.8.0@%s/%s" % (self.user, self.channel))
-
-        if self.options.EIGEN_USE_LAPACKE == True:
-            self.requires("lapack/3.8.0@%s/%s" % (self.user, self.channel))
-
-        if self.options.EIGEN_USE_LAPACKE_STRICT == True:
-            self.requires("lapack/3.8.0@%s/%s" % (self.user, self.channel))
+        pass
 
     def source(self):
         source_url = "http://bitbucket.org/eigen/eigen"
@@ -49,8 +34,8 @@ class EigenConan(ConanFile):
         shutil.move(glob("eigen-eigen-*")[0], self._source_subfolder)
 
     def _configure_cmake(self):
+        
         cmake = CMake(self)
-
         if self.settings.compiler=="gcc":
             if self.settings.arch=="broadwell":
                 cmake.definitions["CMAKE_CXX_FLAGS"] = "-march=broadwell -tune=broadwell"
@@ -67,29 +52,19 @@ class EigenConan(ConanFile):
         with tools.chdir(self._build_subfolder):
             cmake = self._configure_cmake()
             cmake.build()
-            cmake.install()
+
     def package(self):
-        pass
-        #with tools.chdir(self._build_subfolder):
-        #    cmake = self._configure_cmake()
-        #    cmake.install()
+        with tools.chdir(os.path.join(self.build_folder, self._build_subfolder)):
+            # do not reinvoke cmake here
+            self.run("make install")
 
-
+        # additional ressources
+        if self.settings.build_type == 'Debug':
+            self.copy('*.py', src='src/debug/gdb', dst='gdb/eigen', keep_path=True)
+        
     def package_info(self):
         self.cpp_info.includedirs = ['include/eigen3']
 
-        # maybe too intrusive to set here...
-        if self.options.EIGEN_USE_BLAS:
-            self.cpp_info.defines.append("EIGEN_USE_BLAS")
-
-        if self.options.EIGEN_USE_LAPACKE:
-            self.cpp_info.defines.append("EIGEN_USE_LAPACKE")
-
-        if self.options.EIGEN_USE_LAPACKE_STRICT:
-            self.cpp_info.defines.append("EIGEN_USE_LAPACKE_STRICT")
-
-        if self.options.EIGEN_USE_MKL_VML:
-            self.cpp_info.defines.append("EIGEN_USE_MKL_VML")
-
-        if self.options.EIGEN_USE_MKL_ALL:
-            self.cpp_info.defines.append("EIGEN_USE_MKL_ALL")
+        if self.settings.build_type == 'Debug':
+            # case sensitive in cmake
+            self.user_info.GDB_PRINTER = 'gdb'
