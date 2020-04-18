5a59f18c84f7ed0f3eada89508cf0b7aa5535228
jean-mathieu vermosen <vermosen@yahoo.com>
2020-04-18 19:32:46 -0400

    up

--- tws/9.76.1/conanfile.py
+++ tws/9.76.1/conanfile.py
@@ -8,7 +8,8 @@ from conans.errors import ConanInvalidConfiguration
 
 
 class TwsConan(ConanFile):
-    """ Adapted from https://github.com/bincrafters/conan-gtest
+    """ 
+    Adapted from https://github.com/bincrafters/conan-gtest
     """
     name = "tws"
     version = "9.76.1"
@@ -31,67 +32,71 @@ class TwsConan(ConanFile):
         if self.settings.os == "Linux":
             pass
         elif self.settings.os == "Windows":
+            del self.options.fPIC
             pass
         else:
+            raise ConanInvalidConfiguration("os not supported")
             pass
-            #raise ConanInvalidConfiguration("os not supported")
-
+            
     def source(self):
     
-        vers = tools.Version(self.version).as_list
+        vers = Version(self.version).as_list
 
         sha256 = "9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c"
         
         if self.settings.os == "Windows":
-            tools.download("%s/downloads/TWS API Install %s%s.%02d.msi" % (self.homepage, vers[0], vers[1], vers[2]),
-                'install.msi')
-            raise
+            tools.get("%s/downloads/twsapi_macunix.%s%s.%02d.zip" % (self.homepage, vers[0], vers[1], vers[2]))
+            #provided there's a way to handle msi installers...
+            #tools.download("%s/downloads/TWS API Install %s%s.%02d.msi" % (self.homepage, vers[0], vers[1], vers[2]))
 
         elif self.settings.os == "Linux":
             tools.get("%s/downloads/twsapi_macunix.%s%s.%02d.zip" % (self.homepage, vers[0], vers[1], vers[2]))
-            extracted_dirs = {"IBJts/source/cppclient":"tws"}
+        
+        extracted_dirs = {"IBJts/source/cppclient":"tws"}
         
         # create a client and a sample folder
-            for k, v in extracted_dirs.items():
-                shutil.move(k, v)
+        for k, v in extracted_dirs.items():
+           shutil.move(k, v)
         
         # create the cmake file
-            f = open('CMakeLists.txt', 'w+')
-            f.writelines(
-                ['cmake_minimum_required(VERSION 2.8.9)\r\n',
-                 'project(tws)\r\n',
-                 'include_directories(tws)\r\n',
-                 'SET(CMAKE_CXX_STANDARD 11)\r\n'
-                 'file(GLOB SOURCES "tws/client/*.cpp")\r\n',
-                 'file(GLOB PUBLIC_HEADER "tws/client/*.h")\r\n'
-                ])
-        
-            if self.options.shared:
-                f.writelines('add_library(tws SHARED ${SOURCES} ${PUBLIC_HEADER})\r\n')
-            else:
-                f.writelines('add_library(tws STATIC ${SOURCES} ${PUBLIC_HEADER})\r\n')
-        
-                f.writelines(['install(TARGETS tws ARCHIVE DESTINATION lib PUBLIC_HEADER DESTINATION include)'])    
-                f.close()
+        f = open('CMakeLists.txt', 'w+')
+        f.writelines(
+            ['cmake_minimum_required(VERSION 2.8.9)\r\n',
+             'project(tws)\r\n',
+             'include_directories(tws)\r\n',
+             'SET(CMAKE_CXX_STANDARD 11)\r\n'
+             'file(GLOB SOURCES "tws/client/*.cpp")\r\n',
+             'file(GLOB PUBLIC_HEADER "tws/client/*.h")\r\n'
+            ])
+    
+        if self.options.shared:
+            f.writelines('add_library(tws SHARED ${SOURCES} ${PUBLIC_HEADER})\r\n')
+        else:
+            f.writelines('add_library(tws STATIC ${SOURCES} ${PUBLIC_HEADER})\r\n')
+            f.writelines(['install(TARGETS tws ARCHIVE DESTINATION lib PUBLIC_HEADER DESTINATION include)'])    
+            f.close()
         
     def _configure_cmake(self):
         cmake = CMake(self, set_cmake_flags=True)
         return cmake
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.configure()
-        cmake.build()
+        if self.settings.os == "Linux":
+            cmake = self._configure_cmake()
+            cmake.configure()
+            cmake.build()
 
     def package(self):
+        if self.settings.os == "Linux":
+            cmake = self._configure_cmake()
+            cmake.install()
+
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*.pdb", dst="bin", src=".", keep_path=False)
         self.copy(pattern="*.a", dst="lib", src="lib", keep_path=True)
         self.copy(pattern="*.h", dst="include/tws", src="tws/client", keep_path=True)
 
-        cmake = self._configure_cmake()
-        cmake.install()
-
+        
     def package_id(self):
         pass
 
