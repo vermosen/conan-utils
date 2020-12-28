242ef7f79b61d012c3b96e3a578e1b7b4f71682e
jean-mathieu vermosen <vermosen@yahoo.com>
2020-12-28 10:48:19 +0100

    updated soci recipe

--- soci/4.0.0/conanfile.py
+++ soci/4.0.0/conanfile.py
@@ -10,26 +10,39 @@ class SociConan(ConanFile):
     version = "4.0.0"
     license = "Boost Software License - Version 1.0"
     url = "https://github.com/laeknaromur/conan-soci"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
+    settings = "os", "compiler", "build_type", "arch", "cppstd"
+    options = {"lto": [True, False],
+               "fPIC": [True, False],
+               "shared": [True, False],
                "with_db2": [True, False],
                "with_firebird": [True, False],
                "with_mysql": [True, False],
                "with_odbc": [True, False],
                "with_oracle": [True, False],
                "with_postgresql": [True, False],
-               "with_sqlite3": [True, False]}
-    default_options = "shared=False", "with_db2=False", "with_firebird=False",\
+               "with_sqlite3": [True, False],
+               "with_system_boost": [True, False]
+               }
+    
+    default_options = "lto=False", "fPIC=True", \
+        "shared=False", "with_db2=False", "with_firebird=False",\
         "with_mysql=False", "with_odbc=False", "with_oracle=False",\
-        "with_postgresql=False", "with_sqlite3=False"
+        "with_postgresql=False", "with_sqlite3=False", "with_system_boost=False"
     generators = "cmake"
     build_dir = "./"
 
-    def build_requirements(self):
+    def requirements(self):
+
         if self.options.with_sqlite3:
-            self.build_requires("sqlite3/[>3.29.0]@%s/%s" % (self.user, self.channel))
+            self.requires("sqlite3/[>3.29.0]@%s/%s" % (self.user, self.channel))
+
+        if self.options.with_system_boost:
+            pass
+        else:
+            self.requires("boost/[>1.71.0]@%s/%s" % (self.user, self.channel))
             
     def source(self):
+
         tools.download(
             "https://github.com/SOCI/soci/archive/{}.tar.gz".format(self.version),
             "source.tar.gz")
@@ -42,17 +55,45 @@ conan_basic_setup()''')
     def build(self):
         cmake = CMake(self)
 
+        # not sure the gnuXX flags would be handled here...
+        cmake.definitions["CMAKE_CXX_STANDARD"] = self.settings.cppstd
+        
         cmake.definitions["WITH_DB2"] = "ON" if self.options.with_db2 else "OFF"
         cmake.definitions["WITH_FIREBIRD"] = "ON" if self.options.with_firebird else "OFF"
         cmake.definitions["WITH_MYSQL"] = "ON" if self.options.with_mysql else "OFF"
         cmake.definitions["WITH_ODBC"] = "ON" if self.options.with_odbc else "OFF"
         cmake.definitions["WITH_ORACLE"] = "ON" if self.options.with_oracle else "OFF"
         cmake.definitions["WITH_POSTGRESQL"] = "ON" if self.options.with_postgresql else "OFF"
-        cmake.definitions["WITH_SQLITE3"] = "ON" if self.options.with_sqlite3 else "OFF"
 
-        cmake.definitions["BUILD_SHARED_LIBS"] = "ON" if self.options.shared else "OFF"
+        # boost
+        if self.options.with_system_boost:
+            pass
+        else:
+            cmake.definitions["BOOST_INCLUDEDIR"] = self.deps_cpp_info["boost"].include_paths[0]
+            cmake.definitions["BOOST_LIBRARYDIR"] = self.deps_cpp_info["boost"].lib_paths[0]
+        
+        if self.options.with_sqlite3:
+            self.output.info("adding dependency to %s and %s" % (self.deps_cpp_info["sqlite3"].include_paths[0], self.deps_cpp_info["sqlite3"].lib_paths[0]))
+            cmake.definitions["WITH_SQLITE3"] = "ON"
+            cmake.definitions["SQLITE3_INCLUDE_DIR"] = self.deps_cpp_info["sqlite3"].include_paths[0]
+            cmake.definitions["SQLITE3_LIBRARIES"] = "%s/libsqlite3.a" % self.deps_cpp_info["sqlite3"].lib_paths[0]
+        else:
+            cmake.definitions["WITH_SQLITE3"] = "OFF"
+
+        # shared/static
+        cmake.definitions["SOCI_SHARED"] = "ON" if self.options.shared else "OFF"
+        cmake.definitions["SOCI_STATIC"] = "OFF" if self.options.shared else "ON"
+
+        # build type
         cmake.definitions["CMAKE_BUILD_TYPE"] = self.settings.build_type
         cmake.definitions["CMAKE_INSTALL_PREFIX"] = "{}/install".format(self.build_dir)
+
+        # enable lto
+        cmake.definitions["SOCI_LTO"] = "ON" if self.options.lto else "OFF"
+
+        # PIC
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON" if self.options.fPIC else "OFF" 
+
 
         cmake.configure(source_dir="soci-{}".format(self.version),
                         build_dir=self.build_dir)
